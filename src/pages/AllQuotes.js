a9c17c3e527fb9bb30cead61bbcfc747d95999ed
babyazalea <usun12@hotmail.com>
2021-10-12 16:39:52 +0900

    linked to Component

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,16 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "ty", text: "Learning React is fun!" },
+  { id: "q2", author: "ty", text: "Learning React is great!" },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes</h1>;
+  return (
+    <h1>
+      <QuoteList quotes={DUMMY_QUOTES} />
+    </h1>
+  );
 };
 
 export default AllQuotes;
